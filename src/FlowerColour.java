1791c34a97b691d13b4d031234fff740ab11c92b
YuriyVilnitskiy <laba@ucu.edu.ua>
2016-10-23 21:30:10 +0300

    I was trying to use delegation pattern.

---
+++ src/FlowerColour.java
@@ -0,0 +1,16 @@
+/**
+ * Created by jlaba on 23.10.2016.
+ */
+public enum FlowerColour {
+    BLACK, RED, PINK, BLUE;
+    public String toString(){
+        String a = "";
+        switch(this){
+            case BLACK: a = "Black"; break;
+            case RED: a = "Red"; break;
+            case PINK: a = "Pink"; break;
+            case BLUE: a = "Blue"; break;
+            default: a = "Without any colour"; break;
+        }return a;
+    }
+}
