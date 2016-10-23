729b212825eb03d2618d783cebc96a1a0ded2ea1
YuriyVilnitskiy <laba@ucu.edu.ua>
2016-10-23 21:37:45 +0300

    I was trying to use delegation pattern.

---
+++ src/FlowerSpec.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jlaba on 23.10.2016.
+ */
+public class FlowerSpec {
+    protected double length = 0;
+    protected boolean fresh = false;
+    protected double price = 0;
+
+
+
+    public FlowerSpec(double length, boolean fresh, double price){
+        this.price = price;
+
+        this.length = length;
+
+        this.fresh = fresh;
+    }
+
+
+}
